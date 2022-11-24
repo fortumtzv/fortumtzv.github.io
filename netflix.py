e2d23ad3bbd2901a2c768a9c7a599e0ed1f0c775
fortumtzv <88665289+fortumtzv@users.noreply.github.com>
2022-11-24 10:22:30 -0600

    Add files via upload

--- netflix.py
+++ netflix.py
@@ -19,14 +19,14 @@ if agree:
 @st.cache
 def load_data_byname(name):
     data = pd.read_csv(DATA_URL)
-    filtered_data_byname = data[data['name'].str.contains(name)]
+    filtered_data_byname = data[data['name'].str.upper().str.contains(name)]
 
     return filtered_data_byname
 
 myname = sidebar.text_input('Nombre de la pelicula: ')
 btnname = sidebar.button('Buscar por nombre')
 if (btnname):
-    filterbyname = load_data_byname(myname)
+    filterbyname = load_data_byname(myname.upper())
     count_row = filterbyname.shape[0]
     st.write(f"Peliculas Totales: {count_row}")
 
